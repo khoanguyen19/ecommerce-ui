69bdff18e61d936dcd4852d0627141743194e2f6
unknown <khoanguyen1962001@gmail.com>
2022-12-28 13:30:36 +0700

    Fix to local

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://bean-ecommerce-shop.herokuapp.com/api/";
+const BASE_URL = "http://localhost:5000/api";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTM2M2EwMTU2NjQ4OGQxMDEyODliMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NjM5OTg3MiwiZXhwIjoxNjQ2NjU5MDcyfQ.Bj_oHOWhAWKZ5mCT7B05QjmSpEgyI63xa32rhYN5n58";
 
